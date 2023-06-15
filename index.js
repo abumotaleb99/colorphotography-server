9bad2860a5ead807d400cafa082ec2eb756580da
Abu Motaleb <abumotaleb99@gmail.com>
2023-06-16 05:05:40 +0600

    updated post users api

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Enable CORS middleware
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://colorphotography-beefb.web.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -104,7 +118,7 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/users", verifyAdmin, async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
